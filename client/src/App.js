c2745f49d29bd2d0f3ce498a8efeba9d4f4ddfbf
unknown <volkan.goksu@outlook.com>
2021-09-20 16:00:32 +0300

    build tailwindcss

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-red-700">
+        <h2>Volkan</h2>
+    </div>
+  );
+}
+
+export default App;
